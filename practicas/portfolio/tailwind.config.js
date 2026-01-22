a23968bef61ebc328fe3d6a619f78fd3ad0ce834
carlos <alumno.461533@ies-azarquiel.es>
2026-01-22 17:48:53 +0100

    portfolioo

---
+++ practicas/portfolio/tailwind.config.js
@@ -0,0 +1,13 @@
+	
+module.exports = {
+	theme: {
+		extend: {
+			colors: {
+				'yellow-300': '#fde047',
+				'blue-500': '#3b82f6',
+				'blue-700': '#1d4ed8',
+				'blue-900': '#1e3a8a',
+			},
+		},
+	},
+}
